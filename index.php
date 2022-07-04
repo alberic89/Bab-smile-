dd35d35541f0741ef4a3ae57869c3d1555de06a0
alberic89 <90179203+alberic89@users.noreply.github.com>
2022-07-04 19:40:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <html>
-<head>
-<title>run my python files</title>
-<?PHP
-echo shell_exec("python addUrl.py");
-echo it is work;
-?>
-</head>
+    <head><title>Bàb smile +</title></head>
+    <body>
+        <h1>Bàb smile - envoyer un smiley</h1>
+        <form name="submit-url" method="post" action="index.php">
+            Collez l'url complète et non racourcie (ex: "https://brick-a-brack.com/fichiers/images/smileys/001.gif") : <input type="text" name="url"/> <br/>
+            <input type="submit" name="valider" value="Envoyer"/>
+        </form>
+        <?php
+        
+        if(isset($_POST['valider'])){
+			$F=@fopen($url_a_tester,"r");
+			if($F){
+				fclose($F);
+				
+				$url=$_POST['url'];
+				echo $url ;
+			}
+			else {
+				echo '<script>alert("L\'url n\'est pas valide !");</script>';
+			}
+		}
+        
+        ?>
+    </body>
+</html>
+
