7cea86d7dea9dd6ca8eebd010ac13cc6def5b434
alberic89 <90179203+alberic89@users.noreply.github.com>
2022-07-07 17:17:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 $( ".babcode-smiley-container" ).append( "<p><strong>Oh! Des centaines de smileys en plus!!!</strong></p>" );
 
 let data
+let data2
 
-function readTextFile(file, variable) {
+function readTextFile(file) {
 var rawFile = new XMLHttpRequest();
 rawFile.open("GET", file, false);
 rawFile.onreadystatechange = function ()
@@ -12,22 +13,42 @@ rawFile.onreadystatechange = function ()
     {
         if(rawFile.status === 200 || rawFile.status == 0)
         {
-            variable = rawFile.responseText;
+            data = rawFile.responseText;
         }
     }
 }
 rawFile.send(null);
 }
 
-readTextFile("https://raw.githubusercontent.com/alberic89/B-b-smile--/main/ressources/urls.txt", data) ///récupère la dernière version de url.txt
-
+function readTextFile2(file) {
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", file, false);
+rawFile.onreadystatechange = function ()
+{
+    if(rawFile.readyState === 4)
+    {
+        if(rawFile.status === 200 || rawFile.status == 0)
+        {
+            data2 = rawFile.responseText;
+        }
+    }
+}
+rawFile.send(null);
+}
+readTextFile("https://raw.githubusercontent.com/alberic89/B-b-smile--/main/ressources/urls.txt"); ///récupère la dernière version de url.txt
+readTextFile2("https://raw.githubusercontent.com/alberic89/B-b-smile--/main/ressources/customs-urls.txt");
+$(document).ready(function() {
 if (/inbox/.test(document.documentURI) == true && (/talk/.test(document.documentURI)) == false){ ///vérifie que ce n'est pas une page spéciale, et agis en conséquence
 	data.replaceAll('area-babcode','area-message');
+	data2.replaceAll('area-babcode','area-message');
 }
  else{ if (/tutorials_articles/.test(document.documentURI) == true){
 	 data.replaceAll('area-babcode','area-article');
+	 data2.replaceAll('area-babcode','area-article');
 }}
 ///Ajoute les smileys
 $( ".babcode-smiley-container" ).append(data);
 ///Ajoute une autre petite phrase
 $( ".babcode-smiley-container" ).append( "<p><strong>Envoyez un mp à @alberic89 si vous voyez un bug ou si vous voulez ajouter un smiley !</strong></p>" );
+$( ".babcode-smiley-container" ).append(data2); 
+});
