f059fd758af6af238af1b4878ec539afda73cabf
alberic89 <alberic89@gmx.com>
2022-10-14 17:59:06 +0200

    update

---
+++ v4/addCustoms-urls.py
@@ -0,0 +1,15 @@
+url = ''
+url2 = ''
+msg = ''
+while url != 'stop' :
+	print('go')
+	url = str(input())
+	if url != 'stop':
+		url2 = str(input())
+		print('ok')
+	if url != 'stop' and url != '' and url2 != '':
+		msg += '{'+url+';'+url2+'}'
+file=open("customs-urls.txt","a") # ouvre le fichier
+file.write(msg) # écrit le fichier
+file.close()
+print('Done.')
