688fbd6254711b440b7a5b95880164e7876d3c6c
alberic89 <alberic89@gmx.com>
2022-10-15 12:27:33 +0200

    Update addCustoms-urls.py

--- v4/addCustoms-urls.py
+++ v4/addCustoms-urls.py
@@ -1,3 +1,4 @@
+print('Collez l\'url relative')
 url = ''
 url2 = ''
 msg = ''
@@ -8,7 +9,7 @@ while url != 'stop' :
 		url2 = str(input())
 		print('ok')
 	if url != 'stop' and url != '' and url2 != '':
-		msg += '{'+url+';'+url2+'}'
+		msg += '{'+url+';<image>https://brick-a-brack.com'+url2+'</image>}'
 file=open("customs-urls.txt","a") # ouvre le fichier
 file.write(msg) # écrit le fichier
 file.close()
