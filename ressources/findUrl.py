5616e21fca87549fe08d44c0ec717db8c2add77d
alberic89 <alberic89@gmx.com>
2023-01-07 20:38:43 +0100

    update

--- ressources/findUrl.py
+++ ressources/findUrl.py
@@ -1,16 +1,18 @@
 import requests # importe le module
-msg = ''
+msg =''
 def checkUrl(url): # définis la fonction qui permet de tester les urls entrées
 	if requests.head(url).status_code<400: # vérifie que la réponse du serveur contient un code inférieur à 400 (https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP)
 		return True
 	else:
 		return False
+
 for i in range(1,2001): # teste les 2000 premières urls
 	n = str(i)
 	if checkUrl('https://assets.brickfilms.com/emojis/'+n+'.gif')==True:  # si l'url.gif existe, alors l'url est ajoutée à la liste
-		msg += '{'+n+'.gif}'
-	if checkUrl('https://assets.brickfilms.com/emojis/' + n + '.png')==True: # si l'url.png existe, alors l'url est ajoutée à la liste
-		msg += '{'+n+'.png}'
+		msg += '"'+n+'.gif",'
+	elif checkUrl('https://assets.brickfilms.com/emojis/' + n + '.png')==True: # si l'url.png existe, alors l'url est ajoutée à la liste
+		msg += '"'+n+'.png",'
+msg = '[' + msg[:-1] + ']'
 file=open("urls.txt","w") # ouvre le fichier
 file.write(msg) # écrit le fichier
 file.close()
