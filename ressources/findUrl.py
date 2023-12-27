fd4295a9babcc0de789e2725716f553c53f72b73
alberic89 <alberic89@gmx.com>
2023-12-27 17:02:38 +0100

    Update findUrl.py with threads

--- ressources/findUrl.py
+++ ressources/findUrl.py
@@ -1,28 +1,43 @@
-import requests  # importe le module
+import requests
+import concurrent.futures
+import re
 
-msg = []
+MAX_SEARCH = 300
+MAX_THREADS = 300
 
+urls = []
+counter = 0
 
-def checkUrl(url):  # définis la fonction qui permet de tester les urls entrées
-	if requests.head(url).status_code < 400:
-		# vérifie que la réponse du serveur contient un code inférieur à 400 (https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP)
-		return True
-	else:
-		return False
+def trier_liste(liste):
+    def trier_cle(element):
+        chiffres = re.findall(r'\d+', element)
+        return [int(chiffre) for chiffre in chiffres]
+    return sorted(liste, key=trier_cle)
 
+def checkUrl(url):
+    if requests.head(url).status_code < 400:
+        return True
+    else:
+        return False
 
-for i in range(1, 300):  # teste les 300 premières urls
-	n = str(i)
-	if checkUrl("https://assets.brickfilms.com/emojis/" + n + ".gif") == True:
-		# si l'url.gif existe, alors l'url est ajoutée à la liste
-		msg.append(
-			{"u": n + ".gif", "t": "[img]https://assets.brickfilms.com/emojis/" + n + ".gif[/img]"}
-		)
-	elif checkUrl("https://assets.brickfilms.com/emojis/" + n + ".png") == True:
-		# si l'url.png existe, alors l'url est ajoutée à la liste
-		msg.append(
-			{"u": n + ".png", "t": "[img]https://assets.brickfilms.com/emojis/" + n + ".png[/img]"}
-		)
-file = open("urls.txt", "w")  # ouvre le fichier
-file.write(str(msg).replace("'",'"'))  # écrit le fichier
-file.close()
+def generateUrls(nbmax):
+    for i in range(1,nbmax):
+        yield f"https://assets.brickfilms.com/emojis/{i}.gif"
+        yield f"https://assets.brickfilms.com/emojis/{i}.png"
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+    future_to_url = {executor.submit(checkUrl, url): url for url in generateUrls(MAX_SEARCH)}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            isok = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            if isok:
+                urls.append(url)
+                counter += 1
+
+with open("urls.txt", "w") as f:
+    print(*[url for url in trier_liste(urls)], sep="\n", file=f)
+print(counter)
